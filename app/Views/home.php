4b3142587555647c3d160ef67ed08c13541afa4b
Infuseting <tuture27100@gmail.com>
2025-12-04 11:26:17 +0100

    test: See if workflow update

--- app/Views/home.php
+++ app/Views/home.php
@@ -227,7 +227,6 @@ ob_start();
     </div>
 </section>
 
-<!-- Footer -->
 <footer class="bg-gray-900">
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center">
